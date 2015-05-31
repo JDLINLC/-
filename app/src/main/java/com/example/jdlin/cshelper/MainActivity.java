5df9071cdefaa2420c44a94f05fc1cab5eb1ca5b
JDLIN <2837848465@qq.com>
2015-05-31 12:28:33 +0800

    hide title

--- app/src/main/java/com/example/jdlin/cshelper/MainActivity.java
+++ app/src/main/java/com/example/jdlin/cshelper/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.jdlin.cshelper;
 
 import android.content.Intent;
+import android.opengl.GLES31;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.internal.widget.AdapterViewCompat;
@@ -13,58 +14,64 @@ import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 
 
-public class MainActivity extends ActionBarActivity{
+public class MainActivity extends ActionBarActivity {
     private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        toolbar= (Toolbar) findViewById(R.id.toolbar);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        GridView gridView = (GridView)findViewById(R.id.gridview);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        GridView gridView = (GridView) findViewById(R.id.gridview);
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                if(position==0) {
+                if (position == 0) {
                     Intent intent = new Intent(MainActivity.this, SecondActivity.class);
                     startActivity(intent);
+                } else {
+                    Toast.makeText(MainActivity.this, "更新中", Toast.LENGTH_SHORT).show();
                 }
-            } 
+            }
         });
-        ArrayList<HashMap<String,Object>>IstImageItem = new ArrayList<HashMap<String,Object>>();
+        ArrayList<HashMap<String, Object>> IstImageItem = new ArrayList<HashMap<String, Object>>();
 
-        HashMap<String,Object> map1 =new HashMap<String,Object>();
+        HashMap<String, Object> map1 = new HashMap<String, Object>();
         map1.put("ItemImage", R.mipmap.part1);
         map1.put("ItemText", "校内新闻");
         IstImageItem.add(map1);
-        HashMap<String,Object> map2 =new HashMap<String,Object>();
+        HashMap<String, Object> map2 = new HashMap<String, Object>();
         map2.put("ItemImage", R.mipmap.part2);
         map2.put("ItemText", "院系通知");
         IstImageItem.add(map2);
-        HashMap<String,Object> map3 =new HashMap<String,Object>();
+        HashMap<String, Object> map3 = new HashMap<String, Object>();
         map3.put("ItemImage", R.mipmap.part3);
         map3.put("ItemText", "失物招领");
         IstImageItem.add(map3);
-        HashMap<String,Object> map4 =new HashMap<String,Object>();
+        HashMap<String, Object> map4 = new HashMap<String, Object>();
         map4.put("ItemImage", R.mipmap.part4);
         map4.put("ItemText", "扫码签到");
         IstImageItem.add(map4);
-        HashMap<String,Object> map5 =new HashMap<String,Object>();
+        HashMap<String, Object> map5 = new HashMap<String, Object>();
         map5.put("ItemImage", R.mipmap.part5);
         map5.put("ItemText", "吐槽水贴");
         IstImageItem.add(map5);
-        HashMap<String,Object> map6 =new HashMap<String,Object>();
-        map6.put("ItemImage",R.mipmap.part6);
+        HashMap<String, Object> map6 = new HashMap<String, Object>();
+        map6.put("ItemImage", R.mipmap.part6);
         map6.put("ItemText", "更多反馈");
         IstImageItem.add(map6);
 
-        SimpleAdapter saImageItems = new SimpleAdapter(this,IstImageItem,R.layout.item,new String[]{"ItemImage","ItemText"},new int[]{R.id.ItemImage,R.id.ItemText});
+        SimpleAdapter saImageItems = new SimpleAdapter(this, IstImageItem, R.layout.item, new String[]{"ItemImage", "ItemText"}, new int[]{R.id.ItemImage, R.id.ItemText});
         gridView.setAdapter(saImageItems);
     }
 
--- app/src/main/java/com/example/jdlin/cshelper/SecondActivity.java
+++ app/src/main/java/com/example/jdlin/cshelper/SecondActivity.java
@@ -1,13 +1,10 @@
 package com.example.jdlin.cshelper;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Adapter;
-import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.SimpleAdapter;
 
@@ -16,28 +13,32 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class SecondActivity extends ActionBarActivity{
-    private String[] title = new String[]{"1[置顶]","2","3","4","5","6","7","8","9","10"};
-    private String[] content = new String[]{"content1","content2","content3","content4","content5","content6","content7","content8","content9","content10"};
-    private String[] time = new String[]{"2015.5","2015.5","2015.5","2015.5","2015.5","2015.5","2015.5","2015.5","2015.5","2015.5"};
+public class SecondActivity extends ActionBarActivity {
+    private String[] title = new String[]{"1[置顶]", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+    private String[] content = new String[]{"content1", "content2", "content3", "content4", "content5", "content6", "content7", "content8", "content9", "content10"};
+    private String[] time = new String[]{"2015.5", "2015.5", "2015.5", "2015.5", "2015.5", "2015.5", "2015.5", "2015.5", "2015.5", "2015.5"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        GridView gridView_news = (GridView)findViewById(R.id.gridview_news);
-        List<Map<String,Object>> listItems = new ArrayList<Map<String,Object>>();
-        for(int i=0;i<title.length;i++){
-            Map<String,Object> listItem = new HashMap<>();
-            listItem.put("title",title[i]);
-            listItem.put("content",content[i]);
-            listItem.put("time",time[i]);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        GridView gridView_news = (GridView) findViewById(R.id.gridview_news);
+        List<Map<String, Object>> listItems = new ArrayList<Map<String, Object>>();
+        for (int i = 0; i < title.length; i++) {
+            Map<String, Object> listItem = new HashMap<>();
+            listItem.put("title", title[i]);
+            listItem.put("content", content[i]);
+            listItem.put("time", time[i]);
             listItems.add(listItem);
         }
-        SimpleAdapter simpleAdapter = new SimpleAdapter(this,listItems,R.layout.item_news,new String[]{"title","content","time"},new int[]{R.id.news_text_title,R.id.news_text_content,R.id.news_text_time});
+        SimpleAdapter simpleAdapter = new SimpleAdapter(this, listItems, R.layout.item_news, new String[]{"title", "content", "time"}, new int[]{R.id.news_text_title, R.id.news_text_content, R.id.news_text_time});
         gridView_news.setAdapter(simpleAdapter);
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
